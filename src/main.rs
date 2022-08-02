32a55ad012fc11161796b94d112eb82fbae9439e
Jakeman582 <jakehiance@gmail.com>
2022-08-02 12:08:12 -0400

    Updated the readme with lots of information and examples

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use bit_buffers::BitWriter;
+
+fn main() {
+    let mut bit_writer = BitWriter::new();
+
+    bit_writer.write_bit(1);
+    bit_writer.write_bit(0);
+    bit_writer.write_bit(1);
+    bit_writer.write_bit(1);
+    bit_writer.write_bit(1);
+    bit_writer.write_bit(1);
+    bit_writer.write_bit(0);
+    bit_writer.write_bit(1);
+
+    bit_writer.flush_to_file("bit_sequence.bit");
+}
